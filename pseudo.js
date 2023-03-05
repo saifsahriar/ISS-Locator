d755bc6199dc51bbd12b0c1afafb4ffd414070f2
Saif Sahriar <119245305+saifsahriar@users.noreply.github.com>
2023-03-05 18:00:23 +0530

    Add files via upload

---
+++ pseudo.js
@@ -0,0 +1,4 @@
+// this js file has nothing to do with the original program
+// and the icon for ISS you may see is pretty irrelevant but this is the outcome of my laziness to use
+// any valid icon
+// and there is no CSS just because of my laziness, in bengali we call it 'lyad'.
---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+document.getElementById("loading").innerHTML = " we are loading sir....";
+
+//mapping the ISS
+const mymap = L.map('mapId').setView([0, 0], 2);  // 51.505, -0.09  these was the given default data and is now irrelevant
+const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mymap);
+
+
+// mapping the ISS using custom icon
+const icon = L.icon({
+    iconUrl: 'circle.png',   // this rainbow icon as the ISS icon is irrelevant, i know, but this is the result of my laziness
+    iconSize: [20, 20],
+    iconAnchor: [10, 10]
+});
+const marker = L.marker([0, 0], { icon: icon }).addTo(mymap);
+
+let firstTime = true;
+let removeOnce = true;
+
+
+//locating the ISS
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+async function getISS() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    document.getElementById("d1").innerHTML = data.latitude.toFixed(2);
+    document.getElementById("d2").innerHTML = data.longitude.toFixed(2);
+    if (removeOnce) {
+        document.getElementById("loading").remove();
+        removeOnce = false;
+    }
+
+    // these consoles are just for my convenience
+    console.log(data);
+    const { latitude, longitude } = data;
+
+    //locating the ISS at the particular latitude and longitude
+    marker.setLatLng([latitude, longitude]);
+    if (firstTime) {
+        mymap.setView([latitude, longitude], 2);
+        firstTime = false;
+    }
+
+    //just for my convenience
+    console.log(latitude);
+    console.log(longitude);
+}
+
+getISS();
+getISS().catch(error => {
+    console.log("error happened");
+    console.error(error);
+});
+
+setInterval(getISS, 3000);
